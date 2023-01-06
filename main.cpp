b9f17be0d12310573ad52bb753387e68be6ab9d0
angrabdulmoeed <getmoeed@gmail.com>
2023-01-06 11:21:46 +0500

    implemented sign_in in main function

--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
+#include <sign_in>
 using namespace std;
 
 int main(){
-    
+    sign_in();
     return 0;
 }
---
+++ sign_in2.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+int sign_in(){
+    string name, password;
+    cin>> name;
+    cin>> password;
+}
